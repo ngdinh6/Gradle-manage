7a66303b620011dcdf20dc5e3336b8a94f345df6
ngdinh6 <ngdinh304@gmail.com>
2021-06-25 10:58:01 +0700

    25/06/2021 roll back without extent report

--- src/main/java/testcase/BaseTest.java
+++ src/main/java/testcase/BaseTest.java
@@ -1,65 +1,17 @@
 package testcase;
 
 import Constant.Constant;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.MediaEntityBuilder;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import extentreports.ExtentTestManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
-
-import java.io.IOException;
-
 public class BaseTest {
-    public WebDriverWait wait;
-    public WebDriver driver;
-
-    //Click Method
-    public void click(By by) {
-        waitVisibility(by).click();
-    }
-    //Write Text
-    public void writeText(By by, String text) {
-        waitVisibility(by).sendKeys(text);
-    }
-    //Read Text
-    public String readText(By by) {
-        return waitVisibility(by).getText();
-    }
-    //Wait
-    public WebElement waitVisibility(By by) {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-    }
-    // Create global variable which will be used in all method
-    ExtentReports extent;
-    ExtentTest logger;
-
     @Parameters("browserName")
     @BeforeMethod
-    // This code will run before executing any testcase
-    public void setup() {
-        ExtentHtmlReporter reporter = new ExtentHtmlReporter("java/Extent_Report/ExtentReport.html");
-
-        extent = new ExtentReports();
-
-        extent.attachReporter(reporter);
-
-        logger = extent.createTest("TC 01");
-    }
-
-    public void setUp(String browserName) throws Exception {
+    public void setUp( String browserName) throws Exception {
 
         System.out.println("browser: " + browserName);
         if (browserName.equalsIgnoreCase("edge")) {
@@ -78,17 +30,9 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void tearDown(ITestResult result) throws IOException {
-
-        if (result.getStatus() == ITestResult.FAILURE) {
-            String temp = ExtentTestManager.getScreenshot(Constant.WEBDRIVER);
-
-            logger.fail(result.getThrowable().getMessage(), MediaEntityBuilder.createScreenCaptureFromPath(temp).build());
-        }
-
-        extent.flush();
+    public void afterMethod() {
+        System.out.println("Post-Condition");
         Constant.WEBDRIVER.quit();
-
     }
 
 }
--- src/main/java/testcase/TC_01.java
+++ src/main/java/testcase/TC_01.java
@@ -3,7 +3,6 @@ package testcase;
 import Constant.Constant;
 import PageObjects.Railway_HomePage;
 import PageObjects.Railway_LoginPage;
-import logs.Log;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -15,16 +14,16 @@ public class TC_01 extends BaseTest {
 
     @Test
     public void TC01() {
-        Log.info("TC 01 - User can log into Railway with valid username and passowrd");
-        //System.out.println("1. Navigate to QA Railway Website");
+        System.out.println("TC 01 - User can log into Railway with valid username and password");
+        //1. Navigate to QA Railway Website
         Railway_HomePage homePage = new Railway_HomePage();
         homePage.open();
-        //System.out.println("2. Click on \"Login\" tab");
+        //2. Click on Login tab
         Railway_LoginPage loginPage = homePage.gotoLoginPage();
-        //System.out.println("3. Enter valid Email and Password");
-        //System.out.println("4. Click on \"Login\" button");
         JavascriptExecutor jse = (JavascriptExecutor) WEBDRIVER;
         jse.executeScript("window.scrollBy(0,550)");
+        //3. Enter valid Email and Password
+        //4. Click on "Login" button
         loginPage.login(Constant.USERNAME, Constant.PASSWORD);
         String actualMsg = loginPage.getWelcomeMessage();
         String expectedMsg = "Welcome " + Constant.USERNAME;
